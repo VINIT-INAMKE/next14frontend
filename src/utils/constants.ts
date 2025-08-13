2933ac980ed8d8befb9598d03b5c0617b142a419
VINIT-INAMKE <98546938+VINIT-INAMKE@users.noreply.github.com>
2025-08-13 18:21:15 +0530

    Update constants.ts, new Backend Url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import UserData from "@/views/plugins/UserData";
 
 
-export const API_BASE_URL: string = `https://web3lms.onrender.com/api/v1/`;
+export const API_BASE_URL: string = `https://knowledge-ledger.onrender.com/api/v1/`;
 // export const API_BASE_URL: string = `http://127.0.0.1:8000/api/v1/`;
 export const MINT_API_BASE_URL: string = `https://meshmint.vercel.app/`;
 export const userId: number | undefined = UserData()?.user_id;
